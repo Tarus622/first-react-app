1bc71c3c3c1e2b4d75943f7558bb49ee84497cea
Tarus622 <davideoliveira622@gmail.com>
2022-12-04 23:27:33 -0300

    primeiro commit

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logoImage from '../logo.svg'
+
+function Logo() {
+    return (
+        <img src={logoImage} alt= 'Logo'></img>
+    );
+}
+
+export default Logo;
